a487e7c1574072fa71c91b32bf317a87a00ba7b4
abubakarwaheed12 <abubakarwaheed6346528@gmail.com>
2023-02-12 03:37:46 +0500

    ..

--- quiz/urls.py
+++ quiz/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from quiz.views import home , get_quiz
+from quiz.views import home , get_quiz , quiz
 urlpatterns=[
     path('' , home , name='home'),
-    path('api/getquiz' , get_quiz),
+    path('api/getquiz/' , get_quiz),
+    path('quiz/' , quiz),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,17 +1,32 @@
-from django.shortcuts import render , HttpResponse
+from django.shortcuts import render , HttpResponse , redirect
 from quiz.models import Category , Question , Answer
 from django.http import JsonResponse
 import random
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+    category=Category.objects.all()
+    
+    if request.GET.get('cat_name'):
+        cn=request.GET.get('cat_name')
+        return redirect(f'quiz/?cat_name={cn}')
+        
+    context={
+        'category':category,
+    }
+    return render(request, 'index.html', context)
 
-# Male Api
+# Quiz 
+def quiz(request):
+    return render(request, 'quiz.html')
 
+# Make Api
 def get_quiz(request):
     try:
-        question_objs=list(Question.objects.all())
+        question_objs=Question.objects.all()
+        if request.GET.get('cat_name'):
+            question_objs=question_objs.filter(category__category_name__icontains=request.GET.get('cat_name'))
+        question_objs=list(question_objs)
         random.shuffle(question_objs)
         data=[]
         for question_obj in question_objs:
